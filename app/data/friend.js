e83b8b921e0932d4e49db6311a1638b76abb4300
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-05-01 16:40:58 -0700

    some final touches

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,6 @@
 var friendsArray = [{
         name: "Josh",
-        picture: "https://itechway.net/wp-content/uploads/2017/09/geek-boys-whatsapp-dp-1.jpg",
+        photo: "https://itechway.net/wp-content/uploads/2017/09/geek-boys-whatsapp-dp-1.jpg",
         scores: [
             "1",
             "3",
@@ -16,27 +16,27 @@ var friendsArray = [{
     },
     {
         name: "Sam",
-        picture: "https://randomuser.me/api/portraits/men/60.jpg",
+        photo: "https://randomuser.me/api/portraits/men/60.jpg",
         scores: ["4", "2", "3", "1", "5", "4", "1", "1", "3", "2"]
     },
     {
         name: "Kyle",
-        picture: "https://randomuser.me/api/portraits/men/23.jpg",
+        photo: "https://randomuser.me/api/portraits/men/23.jpg",
         scores: ["5", "1", "3", "4", "2", "3", "1", "5", "4", "5"]
     },
     {
         name: "Sidney",
-        picture: "https://randomuser.me/api/portraits/women/54.jpg",
+        photo: "https://randomuser.me/api/portraits/women/54.jpg",
         scores: ["1", "2", "3", "4", "5", "3", "3", "2", "2", "1"]
     },
     {
         name: "Jessica",
-        picture: "https://randomuser.me/api/portraits/women/74.jpg",
+        photo: "https://randomuser.me/api/portraits/women/74.jpg",
         scores: ["5", "5", "5", "3", "3", "3", "2", "5", "5", "5"]
     },
     {
         name: "Erika",
-        picture: "https://randomuser.me/api/portraits/women/89.jpg",
+        photo: "https://randomuser.me/api/portraits/women/89.jpg",
         scores: ["2", "3", "3", "1", "5", "5", "4", "5", "5", "3"]
     }
 ];
